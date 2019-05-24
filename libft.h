7f92f1caaa25126d745d1457ceb66aaa6c3db8bc
Thabang CHEWA <tchewa@c4r12s5.wethinkcode.co.za>
2019-05-24 10:25:17 +0200

    added libft header

---
+++ libft.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 09:31:26 by tchewa            #+#    #+#             */
+/*   Updated: 2019/05/24 10:22:53 by tchewa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_strcat(int c);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strcpy(char *dest, char *src);
+int		ft_strlen(char *str);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+#endif
