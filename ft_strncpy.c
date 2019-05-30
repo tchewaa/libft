cb6b97b8cc6236e9b06c36e3a134d61fe000dae9
Thabang CHEWA <tchewa@c4r12s5.wethinkcode.co.za>
2019-05-30 10:58:06 +0200

    changed conflicting data types

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,15 +6,15 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 10:49:19 by tchewa            #+#    #+#             */
-/*   Updated: 2019/05/24 11:12:02 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/05/30 10:53:09 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, char *src, size_t n)
 {
-	int i;
+	size_t i;
 
 	i = 0;
 	while (i < n)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:31:26 by tchewa            #+#    #+#             */
-/*   Updated: 2019/05/27 09:06:58 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/05/30 10:51:27 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
-int		ft_strcat(int c);
+char	*ft_strcat(char *dest, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, char *src);
-int		ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 char	*ft_strncat(char *dest, const char *src, size_t n);
-char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strncpy(char *dest, char *src, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
