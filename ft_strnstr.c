162d8fd5f9cad484cdc1899751911111c2cca47d
Thabang CHEWA <tchewa@c5r2s5.wethinkcode.co.za>
2019-05-31 11:04:56 +0200

    added ft_strnstr function and modified strlen

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,25 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlen.c                                           :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/20 10:43:33 by tchewa            #+#    #+#             */
-/*   Updated: 2019/05/31 10:49:19 by tchewa           ###   ########.fr       */
+/*   Created: 2019/05/31 09:22:53 by tchewa            #+#    #+#             */
+/*   Updated: 2019/05/31 11:00:50 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t		ft_strlen(const char *str)
+char	*ft_strnstr(const char *str, const char *substr, size_t len)
 {
-	int i;
+	size_t i;
+	size_t j;
+	size_t strlen;
 
 	i = 0;
-	while (str[i] != '\0')
+	strlen = ft_strlen(substr);
+	if (!substr)
+		return ((char *)str);
+
+	while (str[i] && (strlen + i <= len))
 	{
+		j = 0;
+		while (str[i + j] == substr[j])
+		{
+			if (strlen - 1 == j)
+				return ((char *)&str[i]);
+			j++;
+		}
 		i++;
 	}
-	return (i);
+	return (NULL);
 }
