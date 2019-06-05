e82b69e060d49339377d146f27ebcdc02802155d
Charles Chewa <charles.chewa@itschools.co.za>
2019-06-05 22:01:11 +0200

    added ft_memmove function and memmove prototype in the libft

---
+++ ft_memmove.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void    *memmove(void *dest, const void *src, size_t n)
+{
+    size_t i;
+    size_t len;
+    unsigned char *cdest;
+    unsigned char *csrc;
+    char *temp;
+
+    cdest = (unsigned char *)dest;
+    csrc = (unsigned char *)src;
+    i = 0;
+    len = ft_strlen(csrc);
+    temp = (int *) malloc(len * sizeof(n));
+    if (cdest == NULL && csrc == NULL && temp == NULL)
+        return (NULL);
+    ft_memcpy(temp, csrc, n);
+    ft_memcpy(cdest, temp, n);
+    free(temp);
+    return (cdest);
+}
--- libft.h
+++ libft.h
@@ -53,5 +53,6 @@ void	*ft_memset(void *b, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void    *memmove(void *str1, const void *str2, size_t n);
 
 #endif
