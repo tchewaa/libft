21bca599855627f8343ef93ce174f23c0a04c4b6
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-06 14:06:43 +0200

    added function ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 13:26:23 by tchewa            #+#    #+#             */
+/*   Updated: 2019/06/06 14:05:25 by tchewa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t					i;
+	size_t					j;
+	unsigned char			*str1;
+	unsigned char			*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		j = str1[i] - str2[i];
+		if (j != '\0')
+			return (j);
+		i++;
+	}
+	return (0);
+}
