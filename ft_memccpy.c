2cb9767371a83e56ccb9cf8055c814f404fafd6f
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-06 10:26:32 +0200

    added function for memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,33 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchewa <tchewa@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/05 16:26:14 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/05 18:08:38 by tchewa           ###   ########.fr       */
+/*   Created: 2019/06/06 09:32:38 by tchewa            #+#    #+#             */
+/*   Updated: 2019/06/06 10:22:04 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-    size_t i;
-    unsigned char *cdest;
-    unsigned char *csrc;
+	size_t					i;
+	unsigned char			*mdest;
+	unsigned char			*msrc;
 
-    csrc = (unsigned char *)src;
-    cdest = (unsigned char *)dest;
-    i = 0;
-
-	if (csrc == NULL && cdest == NULL)
+	i = 0;
+	msrc = (unsigned char *)src;
+	mdest = (unsigned char *)dest;
+	if (msrc == NULL && mdest == NULL)
 		return (NULL);
-    while (cdest[i] && i < n)
-    {
-        cdest[i] = csrc[i];
-        if (csrc[i] == c)
-            return (&cdest[i + 1]);
-        i++;
-    }
-    return (cdest);
+	while (mdest[i] && i < n)
+	{
+		mdest[i] = msrc[i];
+		if (msrc[i] == c)
+			return (&mdest[i + 1]);
+		i++;
+	}
+	return (NULL);
 }
