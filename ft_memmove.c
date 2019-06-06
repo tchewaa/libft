404e6ed1bb1d4836240140c3ab50a1fa8142632f
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-06 12:20:12 +0200

    modified code to fix errors

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,22 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 09:19:36 by tchewa            #+#    #+#             */
+/*   Updated: 2019/06/06 12:13:09 by tchewa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void    *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    size_t i;
-    size_t len;
-    unsigned char *cdest;
-    unsigned char *csrc;
-    char *temp;
+	char *tmp;
 
-    cdest = (unsigned char *)dest;
-    csrc = (unsigned char *)src;
-    i = 0;
-    len = ft_strlen(csrc);
-    temp = (int *) malloc(len * sizeof(n));
-    if (cdest == NULL && csrc == NULL && temp == NULL)
-        return (NULL);
-    ft_memcpy(temp, csrc, n);
-    ft_memcpy(cdest, temp, n);
-    free(temp);
-    return (cdest);
+	tmp = (char *)malloc(sizeof(char) * n);
+	if (!tmp && !dest && !src)
+		return (NULL);
+	ft_memcpy(tmp, src, n);
+	ft_memcpy(dest, tmp, n);
+	free(tmp);
+	return (dest);
 }
