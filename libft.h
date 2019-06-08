6858b91074b2e7edf6e33c984dad61733bc3986a
Thabang CHEWA <tchewa@c4r9s1.wethinkcode.co.za>
2019-06-08 12:56:10 +0200

    removed forbidden include functions

--- libft.h
+++ libft.h
@@ -6,20 +6,16 @@
 /*   By: tchewa <tchewa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:31:26 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/06 12:59:00 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/06/08 12:54:29 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h>
 # include <string.h>
-# include <ctype.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <wchar.h>
-# include <locale.h>
 
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
@@ -56,5 +52,6 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memalloc(size_t size);
 
 #endif
