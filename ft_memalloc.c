585c12240a26e31d285e46d74896db5a71ecb924
Thabang CHEWA <tchewa@c4r9s1.wethinkcode.co.za>
2019-06-08 12:55:26 +0200

    added new memallocation function

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,16 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/08 11:59:33 by tchewa            #+#    #+#             */
+/*   Updated: 2019/06/08 12:32:42 by tchewa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
 void	*ft_memalloc(size_t size)
 {
 	size_t *ptr;
 
 	ptr = 0;
-	ptr = malloc(size*sizeof(int));
-	if (ptr == NULL)
+	ptr = malloc(size * sizeof(int));
+	if (!ptr)
+	{
 		return (NULL);
-	else
-		return (ptr);
-	free(ptr);
+	}
+	ft_bzero(ptr, size);
+	return (ptr);
 }
