5312012aa13b1567c69cab3495e8f6b618b77896
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-15 00:11:35 +0200

    fixed unitest

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 09:32:38 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/08 17:13:45 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/06/15 00:09:32 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,11 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	i = 0;
 	msrc = (unsigned char *)src;
 	mdest = (unsigned char *)dest;
-	if (msrc == NULL && mdest == NULL)
-	{
-		return (NULL);
-	}
-	while (mdest[i] && i < n)
+	while (i < n)
 	{
 		mdest[i] = msrc[i];
-		if (msrc[i] == c)
-			return (&mdest[i + 1]);
+		if (mdest[i] == (unsigned char )c)
+			return (mdest + i + 1);
 		i++;
 	}
 	return (NULL);
