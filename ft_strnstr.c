e9e0977f6d08cbed8905ed24024c26ca58af3232
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-20 16:49:56 +0200

    code fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,30 +6,35 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:22:53 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/15 04:00:24 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/06/20 16:47:02 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *substr, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t i;
 	size_t j;
 	size_t strlen;
 
 	i = 0;
-	strlen = ft_strlen(substr);
-	if (!substr)
-		return ((char *)str);
-	while (str[i] && (strlen + i <= len))
+	strlen = ft_strlen(needle);
+	if (strlen == 0)
+		return ((char *)&haystack[i]);
+	if (len == 0)
+		return (NULL);
+	if (needle[i] == '\0')
+		return ((char *)&haystack[i]);
+	while (haystack[i] && (i < len))
 	{
 		j = 0;
-		while (str[i + j] == substr[j])
+		while (haystack[i + j] == needle[j] && i < len)
 		{
 			if (strlen - 1 == j)
-				return ((char *)&str[i]);
+				return ((char *)&haystack[i]);
 			j++;
+			len--;
 		}
 		i++;
 	}
