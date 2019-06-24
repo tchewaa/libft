2046ab9f1775617445b4809c030f05510b3df79f
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-24 10:18:43 +0200

    string split function

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: tchewa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 10:30:52 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/22 01:55:02 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/06/24 09:46:21 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,34 +47,26 @@ char				**ft_strsplit(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	size_t	k;
 	char	**str;
 	size_t	words;
-	size_t	letters;
 
 	i = 0;
 	j = 0;
+	if (!s)
+		return (NULL);
 	words = count_words((char*)s, c);
 	if (!(str = (char **)malloc(sizeof(char*) * words)))
 		return (NULL);
 	while (s[i] != '\0')
 	{
-		while ((char)s[i] == c && s[i] != '\0')
-			i++;
-		while (j < words && s[i] != c && s[i] != '\0')
+		if (s[i] == c && s[i] != '\0')
 		{
-			k = 0;
-			letters = count_letters(((char *)s + i), c);
-			str[j] = (char*)malloc(sizeof(char) * (letters + 1));
-			while (s[i] != c && s[i] != '\0')
-			{
-				str[j][k] = (char)s[i];
-				i++;
-				k++;
-			}
-			str[j][k] = '\0';
+			str[j] = ft_strsub(s, i, count_letters(((char *)s + i), c));
 			j++;
+			i = i + count_letters((char*)s + i + c);
 		}
+		while (s[i] == c && s[i] != '\0')
+			i++;
 	}
 	return (str);
 }
