5d8d13a67e4ff16b0db78c34785a94ef965edd72
Thabang CHEWA <tchewa@c4r12s1.wethinkcode.co.za>
2019-06-27 15:18:05 +0200

    upddate libft.h file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tchewa <tchewa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 09:31:26 by tchewa            #+#    #+#             */
-/*   Updated: 2019/06/25 12:22:49 by tchewa           ###   ########.fr       */
+/*   Updated: 2019/06/27 15:17:31 by tchewa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,12 @@ int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
-char		*ft_strcat(char *dest, const char *src);
+char		*ft_strcat(char *dst, const char *src);
 int			ft_strcmp(const char *s1, const char *s2);
-char		*ft_strcpy(char *dest, const char *src);
+char		*ft_strcpy(char *dst, const char *src);
 size_t		ft_strlen(const char *str);
-char		*ft_strncat(char *dest, const char *src, size_t n);
-char		*ft_strncpy(char *dest, const char *src, size_t n);
+char		*ft_strncat(char *dst, const char *src, size_t n);
+char		*ft_strncpy(char *dst, const char *src, size_t n);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -47,9 +47,9 @@ void		ft_putendl(char const *s);
 void		ft_putendl_fd(char const *s, int fd);
 void		*ft_memset(void *b, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t len);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memalloc(size_t size);
